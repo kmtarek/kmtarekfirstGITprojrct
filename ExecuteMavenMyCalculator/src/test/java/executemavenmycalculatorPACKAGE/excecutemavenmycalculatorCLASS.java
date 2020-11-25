752008a30f07f0114082783e0bbc6021f38c9ee2
kmtarek <kmtarekfca@gmail.com>
2020-11-25 18:21:37 -0500

    fourth commit

---
+++ ExecuteMavenMyCalculator/src/test/java/executemavenmycalculatorPACKAGE/excecutemavenmycalculatorCLASS.java
@@ -0,0 +1,16 @@
+package executemavenmycalculatorPACKAGE;
+
+import com.packagetarek.august.maven.MavenMyCalculator;
+
+public class excecutemavenmycalculatorCLASS {
+
+	public static void main(String[] args) {
+		
+		
+		MavenMyCalculator m = new MavenMyCalculator();
+		
+		
+
+	}
+
+}
